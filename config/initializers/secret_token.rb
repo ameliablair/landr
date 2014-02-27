a613156b369a24168a97bacc2e93084df9ed75ac
ameliablair <ameliablair@gmail.com>
2014-02-26 19:00:18 -0500

    still trying to add all new files.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Landr::Application.config.secret_key_base = '9d52458467417281b6c7ce485acbd1cab4f3aa37f7a5f75f319017d29d89bba45282989c3c36f01b739102879116c78f4367d70692c95ab4088d56dc95cec98a'
